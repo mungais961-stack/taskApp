dea288a09c44fda223c0293560366faaef226d1c
MungaiStack <mungais961@gmail.com>
2026-08-03 20:06:06 +0300

    The first commit.

---
+++ src/main/java/com/simon/taskapp/domain/entities/enums/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.simon.taskapp.domain.entities.enums;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/simon/taskapp/domain/entities/enums/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.simon.taskapp.domain.entities.enums;
+
+public enum TaskStatus {
+    OPEN,
+    CLOSED
+}
